bea5849b21a2059c342996538ba0552f32def030
JayantGaikwaddib <jayant@digitalinfobytes.com>
2024-06-03 12:01:50 +0530

    commit for development conflict

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,10 @@ namespace Project.Controllers
         {
             return View();
         }
+        public IActionResult Privacy222()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
